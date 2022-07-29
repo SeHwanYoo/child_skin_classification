f7b54e60955c7899738ded5107eb5a147a8d43df
Sehwan Yoo <krstyle03v@gmail.com>
2022-07-29 16:54:54 +0900

    .

--- main.py
+++ main.py
@@ -30,15 +30,17 @@ warnings.filterwarnings(action='ignore')
 from silence_tensorflow import silence_tensorflow
 silence_tensorflow()
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        # Currently, memory growth needs to be the same across GPUs
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-    except RuntimeError as e:
-        # Memory growth must be set before GPUs have been initialized
-        print(e)
+tf.debugging.set_log_device_placement(True)
+
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# if gpus:
+#     try:
+#         # Currently, memory growth needs to be the same across GPUs
+#         for gpu in gpus:
+#             tf.config.experimental.set_memory_growth(gpu, True)
+#     except RuntimeError as e:
+#         # Memory growth must be set before GPUs have been initialized
+#         print(e)
 
 
 num_res = 300
